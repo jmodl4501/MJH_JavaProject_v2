dd034b1c9696d3e7591b20224c330924065b01cd
jmodl4501 <x2twoi@naver.com>
2022-06-13 09:59:04 +0900

    batch_insert

---
+++ web01/src/cmdLog/gitClone_boot.bat
@@ -0,0 +1,10 @@
+rmdir D:\Project\MJH_JavaProject_v2 /s /q
+rmdir D:\Project\JavaWebProgramming /s /q
+D:
+cd D:\Project
+git init D:\Project
+git clone https://github.com/jmodl4501/MJH_JavaProject_v2.git
+git clone https://github.com/eomjinyoung/JavaWebProgramming.git
+cd D:\Project\MJH_JavaProject_v2
+chcp 65001
+echo %date% %time%^>파일생성> D:\Project\MJH_JavaProject_v2\web01\src\cmtLog\%date%.txt
