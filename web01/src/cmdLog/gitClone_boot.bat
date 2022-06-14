f0c2aca8cbec9d33fd730942caf7bdc0e261f782
jmodl4501 <x2twoi@naver.com>
2022-06-14 18:04:32 +0900

    220614_1804

--- web01/src/cmdLog/gitClone_boot.bat
+++ web01/src/cmdLog/gitClone_boot.bat
@@ -7,7 +7,7 @@ git clone https://github.com/jmodl4501/MJH_JavaProject_v2.git
 git clone https://github.com/eomjinyoung/JavaWebProgramming.git
 cd D:\Project\MJH_JavaProject_v2
 chcp 65001
-echo %date% %time%^>파일생성> D:\Project\MJH_JavaProject_v2\web01\src\cmdLog\%date%.txt
+echo %date% %time%^>파일생성> D:\Project\MJH_JavaProject_v2\web01\src\cmdLog\%date:-=%^_%time:~0,2%%time:~3,2%%time:~6,2%.txt
 pause
 git init D:\Project\MJH_JavaProject_v2
 git add .
