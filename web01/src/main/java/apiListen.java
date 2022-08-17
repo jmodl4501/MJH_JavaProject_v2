cdf68f459c85bc012ad02bcd121dbffaf691d7cd
jmodl4501 <x2twoi@naver.com>
2022-08-17 18:04:56 +0900

    api insert

--- web01/src/main/java/apiListen.java
+++ web01/src/main/java/apiListen.java
@@ -2,6 +2,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 
 public class apiListen {
@@ -12,14 +24,45 @@ public class apiListen {
 
 	private static void getJson() {
 		try {
-			String api_token 		= "";
+			String api_token 		= "tAepjYM6f6AWf6GY";
 			String searchCompleteSD = "2022-06-28";
 			String searchCompleteED	= "2022-06-29";
+			String searchStep		= "7";
 			String getUrl = "https://privacy.kda.or.kr/admin/agreement/answer_api.php"
-							+ "?api_token=" 		+ api_token 
+							+ "?api_token=" 		+ api_token
+							+ "&agency="		+ searchStep							
+							+ "&searchStep="		+ searchStep							
 							+ "&searchCompleteSD="	+ searchCompleteSD
 							+ "&searchCompleteED="	+ searchCompleteED;
 			URL url = new URL(getUrl);
+			
+			TrustManager[] trustAllCerts = new TrustManager[] {
+				new X509TrustManager() {
+					
+					@Override
+					public X509Certificate[] getAcceptedIssuers() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					@Override
+					public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+						// TODO Auto-generated method stub
+						
+					}
+				}	
+			};
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			
+			System.out.println(url);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			
 			conn.setRequestMethod("GET"); // http 메서드
@@ -36,8 +79,12 @@ public class apiListen {
 				sb.append(line);
 			}
 			
-			//JSONObject obj = new JSONObject(sb.toString()); // json으로 변경
-			//System.out.println("code = " + obj.getInt("code") + " / message= " + obj.getString("message"));
+			JSONObject obj 		= (JSONObject) new JSONParser().parse(sb.toString()); // json으로 변경
+			JSONArray arrData	= (JSONArray) obj.get("data");
+			System.out.println(sb.toString());
+			System.out.println("status= " + obj.get("status"));
+			
+			System.out.println("cntData= " + arrData.size());			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
